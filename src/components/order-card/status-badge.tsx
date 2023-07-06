e290a2622d3a1ae90c2b1b66229faf538a8d88d5
SamuelRnn <ricardo.reinn@outlook.com>
2023-07-06 18:19:55 -0500

    status badge icons

--- src/components/order-card/status-badge.tsx
+++ src/components/order-card/status-badge.tsx
@@ -1,11 +1,20 @@
 import getStatusLabel from '@/helpers/get-status-label'
 import { OrderStatus } from '@/interfaces/order.interface'
-import { css, styled } from 'styled-components'
+import { styled } from 'styled-components'
+import { BiLoaderAlt } from 'react-icons/bi'
+import { AiFillClockCircle } from 'react-icons/ai'
+import { BsXLg } from 'react-icons/bs'
+import { MdOutlineDone } from 'react-icons/md'
 
 const StyledStatusBadge = styled.span`
 	border-radius: 10rem;
 	padding: 0.25rem 0.75rem;
 	user-select: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 0.5rem;
+	font-weight: normal;
 
 	&[data-variant='pending'] {
 		color: var(--status-pending);
@@ -24,11 +33,37 @@ const StyledStatusBadge = styled.span`
 		border: 1px solid var(--status-finished);
 	}
 `
+const AnimatedProgresssIcon = styled(BiLoaderAlt)`
+	@keyframes rotate {
+		0% {
+			rotate: 0deg;
+		}
+		100% {
+			rotate: 360deg;
+		}
+	}
+	animation-name: rotate;
+	animation-duration: 1.2s;
+	animation-iteration-count: infinite;
+	animation-timing-function: linear;
+`
+
+const statusIconsMap = {
+	progress: <AnimatedProgresssIcon />,
+	pending: <AiFillClockCircle />,
+	canceled: <BsXLg />,
+	finished: <MdOutlineDone />,
+}
 
 interface Props {
 	status: OrderStatus
 }
 
 export default function Badge({ status }: Props) {
-	return <StyledStatusBadge data-variant={status}>{getStatusLabel(status)}</StyledStatusBadge>
+	return (
+		<StyledStatusBadge data-variant={status}>
+			{getStatusLabel(status)}
+			{statusIconsMap[status]}
+		</StyledStatusBadge>
+	)
 }
